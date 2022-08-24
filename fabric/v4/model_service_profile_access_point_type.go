22029d4ebe75b0c4f29994702920221e364c55a6
Srinivasa Yedluri <syedluri@equinix.com>
2022-08-24 16:42:18 -0700

    Addressing inheritance issue with go lang sdk generator

--- fabric/v4/model_service_profile_access_point_type.go
+++ fabric/v4/model_service_profile_access_point_type.go
@@ -11,8 +11,27 @@ package v4
 
 // Access Point Type
 type ServiceProfileAccessPointType struct {
-	Type_ *ServiceProfileAccessPointTypeEnum `json:"type"`
-	Uuid  string                             `json:"uuid,omitempty"`
-	ServiceProfileAccessPointTypeColo
-	ServiceProfileAccessPointTypeVd
+	Type_               *ServiceProfileAccessPointTypeEnum `json:"type"`
+	Uuid                string                             `json:"uuid,omitempty"`
+	SupportedBandwidths *[]int32                           `json:"supportedBandwidths,omitempty"`
+	// Setting to allow or prohibit remote connections to the service profile.
+	AllowRemoteConnections bool `json:"allowRemoteConnections,omitempty"`
+	// Setting to enable or disable the ability of the buyer to customize the bandwidth.
+	AllowCustomBandwidth bool `json:"allowCustomBandwidth,omitempty"`
+	// percentage of port bandwidth at which an allocation alert is generated - missing on wiki.
+	BandwidthAlertThreshold float64 `json:"bandwidthAlertThreshold,omitempty"`
+	// Setting to enable or disable the ability of the buyer to change connection bandwidth without approval of the seller.
+	AllowBandwidthAutoApproval bool `json:"allowBandwidthAutoApproval,omitempty"`
+	// Availability of a bandwidth upgrade. The default is false.
+	AllowBandwidthUpgrade bool                              `json:"allowBandwidthUpgrade,omitempty"`
+	LinkProtocolConfig    *ServiceProfileLinkProtocolConfig `json:"linkProtocolConfig,omitempty"`
+	// for verizon only.
+	EnableAutoGenerateServiceKey bool `json:"enableAutoGenerateServiceKey,omitempty"`
+	// Mandate redundant connections
+	ConnectionRedundancyRequired bool       `json:"connectionRedundancyRequired,omitempty"`
+	ApiConfig                    *ApiConfig `json:"apiConfig,omitempty"`
+	// custom name for \"Connection\"
+	ConnectionLabel   string                  `json:"connectionLabel,omitempty"`
+	AuthenticationKey *AuthenticationKey      `json:"authenticationKey,omitempty"`
+	Metadata          *ServiceProfileMetadata `json:"metadata,omitempty"`
 }
